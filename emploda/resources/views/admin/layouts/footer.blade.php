dedf0047f072472c0cdd54b3f0f532eb3cba9994
Sapae Safae <sapaebaamel@gmail.com>
2021-07-13 19:58:09 +0100

    updates

---
+++ emploda/resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Made with &#10084; Sbaamel</div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{asset('template/css/js/scripts.js')}}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="{{asset('template/css/assets/demo/chart-area-demo.js')}}"></script>
+        <script src="{{asset('template/css/assets/demo/chart-bar-demo.js')}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="{{asset('template/css/js/datatables-simple-demo.js')}}"></script>
+    </body>
+</html>
